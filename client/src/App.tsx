fcb77c6199fc744903bd32fb1c9c7ecd8f1d6a5b
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-08-11 10:59:16 +0900

    delete update  구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios'
 import './App.css';
-import axios from 'axios';
-import { Type } from 'typescript';
 
 function App() {
   interface movieData{ID:number,movieName:string,movieReview:string}
   const [movieName,setMovieName] = useState('')
   const [movieReview,setMovieReview] = useState('')
   const [movieList,setMovieList] = useState<movieData[]>([])
+  const [newReview, setNewReview] = useState('')
 
-
-  useEffect(()=>{
+  useEffect( ()=>{
     Axios.get(`http://localhost:3001/api/get`).then((res)=>{
       setMovieList(res.data)
     })
   },[])
 
-  const submitReview = ():any=>{
+  const submitReview =  ()=>{
     if(movieName === '') return alert('영화제목을 입력하세요')
     if(movieReview === '') return alert('리뷰를 입력하세요') 
     Axios.post(`http://localhost:3001/api/insert`,{
       movieName:movieName,
       movieReview:movieReview
-    }).then(()=>{
-      alert("success")
-      setMovieList([...movieList,{ID:movieList.length + 100,movieName:movieName,movieReview:movieReview}])
+    }).then((res)=>{
+      alert("insert success")
+      setMovieList([...movieList,{
+        ID:res.data[0].ID , movieName:movieName,movieReview:movieReview
+      }])
       setMovieName('')
       setMovieReview('')
+    }).catch(()=>{
+      alert('insert error 관리자에 문의')
     })
   }
-  const deleteReview = (movieName:any):any => {
-    Axios.delete(`http://localhost:3001/api/delete/${movieName}`)
+
+  const deleteReview = (ID:number):any => {
+    Axios.delete(`http://localhost:3001/api/delete/${ID}`)
+    .then((res)=>{alert("delete success")})
+    .catch(()=>{alert('delete error 관리자에 문의')})
+    const newMovieList=movieList.filter((val)=>{return val.ID !== ID});
+    setMovieList(newMovieList)
+  }
+
+  const updateReview = (ID:number,newReview:string):any=> { 
+    if(!newReview)return alert('리뷰를 입력하세요')
+    Axios.put(`http://localhost:3001/api/update`,{ID:ID,newReview:newReview})
+    .then(()=>{
+      // eslint-disable-next-line array-callback-return
+      const newList = movieList.map(val=>{if(val.ID === ID){val.movieReview = newReview} return val})
+      setMovieList(newList)
+      setNewReview('')
+      alert('update success')
+    }).catch(()=>{
+      setNewReview('')
+      alert('update error 관리자에 문의')
+    })
   }
 
   return (
@@ -52,9 +74,15 @@ function App() {
         return (<div key={value.ID} className="card"> 
         <h1>movie: {value.movieName}</h1>
         <p>review: {value.movieReview}</p>
-        <input type="text" placeholder='review' id='reviewUpdate'></input>
-        <button>Update</button>
-        <button onClick={()=>deleteReview(value.movieName)}>Delete</button>
+        <input 
+          type="text" 
+          placeholder='review' 
+          id='reviewUpdate'
+          value={newReview}
+          onChange={(e)=>{setNewReview(e.target.value)}}>
+        </input>
+        <button onClick={()=>updateReview(value.ID,newReview)}>Update</button>
+        <button onClick={()=>deleteReview(value.ID)}>Delete</button>
         </div>)
       })}
       </div> 
--- server/index.js
+++ server/index.js
@@ -3,12 +3,22 @@ const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
+const { rejects } = require('assert');
+
 const db = mysql.createPool({
     host: 'localhost',
     user: 'nah0101',
     password: 'cjdruf0984~',
     database: 'crud'
 })
+
+const listGet = (res,column,where)=>{
+    const sqlselect = `select ${column} from movie_review ${where}`
+    db.query(sqlselect, (err, result) => {  
+        if(err) {   throw err }
+        res.status(200).send(result);
+    })
+}
 
 app.use(cors())
 app.use(express.json())
@@ -17,37 +27,55 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+
 app.get('/api/get',(req,res)=>{
-    const sqlselect = "select * from moviereview"
-    db.query(sqlselect, (err, result) => {  
-        if(err) {   throw err }
-        console.log(result);
-        res.status(200).send(result);
-    })
+    listGet(res,'*','')
 })
 
 app.post('/api/insert', (req ,res)=>{
     const movieName = req.body.movieName
     const movieReview = req.body.movieReview
-    const sqlinsert = "INSERT INTO movieReview (movieName,movieReview) values (?,?)"
+    const sqlinsert = "INSERT INTO movie_review (movieName,movieReview) values (?,?)"
     db.query(sqlinsert,[movieName ,movieReview], (err,result) =>{
         if(err){
-            throw err;
+            console.log(err);
+            rejects(err) ;
+        }else{
+            listGet(res,'ID',`order by ID DESC LIMIT 1`)
         }
-        console.log(result);
-        res.send('insert successfully')
     })
 })
 
-app.delete('/api/delete/:movieName',(req,res)=>{
-    res.send('hello')
-    const name = req.params.movieName;
-    console.log(name);
-    const sqlDelete = `delete from moviereview where movieName = ? `
-    db.query(sqlDelete,[name], (err, result) => {
-        if(err){throw err}
-        console.log('success')
+app.delete('/api/delete/:ID',(req,res)=>{
+    const ID = req.params.ID;
+    const sqlDelete = `delete from movie_review where ID = ? `
+    db.query(sqlDelete,[ID], (err, result) => {
+        if(err){
+            console.log(err);
+            reject(err)
+        }else{
+            console.log('delete finished')
+            res.status(200).send(result);
+        }
+    })
+})
+
+app.put('/api/update',(req,res)=>{
+    console.log('server update')
+    const ID = req.body.ID
+    const review = req.body.newReview
+    const sqlUpdate = `update movie_review set movieReview = ? where ID = ?`
+    db.query(sqlUpdate,[review,ID],(err, result)=>{
+        if(err){
+            console.log(err);
+            rejects(err)
+        }else{
+            console.log('update finished')
+            res.status(200).send(result);
+        }
     })
 })
 
 app.listen(3001 , ()=> {console.log("3001 server" )})
+
+//같은것 선택 컨트롤 + D , 알트 + 마우스 클릭 
